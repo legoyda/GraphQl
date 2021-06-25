46982e4cdaface957cc772741684a023682d2832
Vlad <54063227+legoyda@users.noreply.github.com>
2021-06-25 12:11:49 +0300

    POST  ... ok Create Note ... ok
    Delete Note ... in process

--- api.py
+++ api.py
@@ -1,10 +1,11 @@
 import graphene
-from graphene import Int, String, Schema, List, ObjectType
+from graphene import Int, String, Schema, List, ObjectType, Field, Mutation
 from fastapi import FastAPI
 from starlette.graphql import GraphQLApp
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import model
+from model import Base, Notes
 
 engine = create_engine("sqlite:///back.db")
 session_local = sessionmaker(engine)
@@ -12,21 +13,63 @@ model.Base.metadata.create_all(engine)
 
 
 class NotesType(ObjectType):
-    id = Int()
-    title = String()
-    description = String()
+    id = Int(required=True)
+    title = String(required=True)
+    description = String(required=True)
 
 
-class Query(ObjectType):
-    hello = String(name=String(default_value="Stranger"))
+class View(ObjectType):
     get_notes = List(NotesType)
 
-    def resolve_get_notes(self,info):
+    def resolve_get_notes(self, info):
         session = session_local()
-        result = session.query(model.Notes).all()\
-
+        result = session.query(model.Notes).all()
         return result
 
 
+class CreateNote(Mutation):
+    id = graphene.Int()
+    title = graphene.String()
+    description = graphene.String()
+
+    class Arguments:
+        title = graphene.String()
+        description = graphene.String()
+
+    def mutate(self, info, title, description):
+        new_note = Notes(title=title, description=description)
+        session = session_local()
+        session.add(new_note)
+        session.commit()
+        return CreateNote(id=new_note.id, title=new_note.title, description=new_note.description)
+
+
+class Mutation(ObjectType):
+    create_note = CreateNote.Field()
+
+"""
+class DeleteNote(Mutation):
+    id = graphene.Int()
+    title = graphene.String()
+    description = graphene.String()
+
+    class Arguments:
+        id = graphene.Int()
+
+        def mutate(self, id):
+            session = session_local()
+            note = session.query(Notes).get(id)
+            session.delete(note)
+            session.commit()
+            return DeleteNote(id=note.id)
+
+
+class DeletNote(ObjectType):
+    delete_note = DeleteNote.id
+"""
+
 app = FastAPI()
-app.add_route("/", GraphQLApp(schema=Schema(query=Query)))
+app.add_route("/", GraphQLApp(
+    schema=Schema(query=View, mutation=Mutation)
+)
+              )
